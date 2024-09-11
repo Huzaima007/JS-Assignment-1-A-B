fc1d7791fc31248cd59e119ffd0dc35fe5c6f4da
Huzaima007 <huzaimasiddiq17@gmail.com>
2024-09-11 20:44:19 +0500

    Update App.js

---
+++ Assets/App.js
@@ -0,0 +1,54 @@
+// Chapter 1 ALERTS
+
+// Qusetion 1
+
+// alert("Well Come To Our Website!");
+
+// Qusetion 2
+
+// alert("Error! Please Enter A Valid Password");
+
+// Qusetion 3
+
+// alert("Wellcome To JS Land \n Happy Coding!");
+
+// // Qusetion 4
+
+// alert("Wellcome To JS Land");
+// alert("Happy Coding");
+// alert("I Hope You Enjoy your Coding!");
+
+// Qusetion 5
+
+console.log("Hello I Can Run JS Through My Browser Console");
+
+// Qusetion 6
+// ?
+// Qusetion 7
+// ?
+
+// Chapter 2 VARIABLES FOR STRINGS
+
+// Qusetion 1
+
+var username;
+
+// Qusetion 2
+
+var myName = "Your Full Name";
+
+// Qusetion 3
+
+var message = "Hello World";
+alert(message);
+
+// Qusetion 4
+
+var name1 = "Huzaima";
+var age = "15";
+var certified = "Front End Web Developer";
+alert(name1);
+alert(age);
+alert(certified);
+
+// Qusetion 5
