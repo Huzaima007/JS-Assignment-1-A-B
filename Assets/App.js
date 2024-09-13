59baaeaa9a6ade3a1d73e2d7a62e92a3c9eb289a
Huzaima007 <huzaimasiddiq17@gmail.com>
2024-09-13 15:52:26 +0500

    Update App.js

--- Assets/App.js
+++ Assets/App.js
@@ -20,7 +20,7 @@
 
 // Qusetion 5
 
-console.log("Hello I Can Run JS Through My Browser Console");
+// console.log("Hello I Can Run JS Through My Browser Console");
 
 // Qusetion 6
 // ?
@@ -31,24 +31,115 @@ console.log("Hello I Can Run JS Through My Browser Console");
 
 // Qusetion 1
 
-var username;
+// var username;
 
 // Qusetion 2
 
-var myName = "Your Full Name";
+// var myName = "Your Full Name";
 
 // Qusetion 3
 
-var message = "Hello World";
-alert(message);
+// var message = "Hello World";
+// alert(message);
 
 // Qusetion 4
 
-var name1 = "Huzaima";
-var age = "15";
-var certified = "Front End Web Developer";
-alert(name1);
-alert(age);
-alert(certified);
+// var name1 = "Huzaima";
+// var age = "15";
+// var certified = "Front End Web Developer";
+// alert(name1);
+// alert(age);
+// alert(certified);
 
 // Qusetion 5
+
+// var Pizza = " Pizza \n Pizz \n Piz \n Pi \n P";
+// alert(Pizza);
+
+// Qusetion 6
+
+// var email = "huzaima@gmail.com";
+// alert("My email address is " + email);
+
+// Qusetion 7
+
+// var book = "A Smarter Way To Learn Javascript";
+// alert(book);
+
+// Qusetion 8
+
+// ?
+
+// Qusetion 9
+
+// var designString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(designString);
+
+// Chapter 3
+
+// Question 1
+
+// var age = 15;
+// alert("I Am " + age + "Years Old");
+
+// Qusetion 2
+
+// var visit = 15;
+// alert("You have visited this site " + visit + " times”.");
+
+// Qusetion 3
+
+// var birthYear = 2009;
+// alert("My birth year is " + birthYear);
+
+// Qusetion 4
+
+// var visitorName = "Huzaima";
+// var productName = "Polo Shirt";
+// var quantity = 5;
+
+// console.log(
+//   visitorName +
+//     " ordered " +
+//     quantity +
+//     " " +
+//     productName +
+//     "(s) on XYZ Clothing store."
+// );
+
+// Chapter 4
+
+// Question 1
+
+// var name1 = "Huzaima",
+//   dob = 2009,
+//   Fn = "Siddiq";
+// console.log(name1 + dob + Fn);
+
+// Question 2
+
+//   ----------------------Legal Variable Names------------------------
+
+// name;
+// age;
+// totalAmount;
+// isActive;
+// score;
+
+// ----------------------Illegal Variable Names------------------------
+
+// 1stName
+// user name
+// var
+// total-amount
+// @score
+
+// Question 3
+
+var rules = "Rules for naming JS variables:";
+var a = " Variable names can only contain: numbers, $ and _";
+var b =
+  " Variables must begin with a letter, $, or _. For example: $name, _name or name";
+var c = " Variable names are case sensitive";
+var d = " Variable names should not be JS keywords";
+console.log(rules + a + b + c + d);
